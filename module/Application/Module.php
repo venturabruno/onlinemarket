5a1aa5287b85b9e01369cf69a50fcd00cac9e0f5
Bruno Ventura <venturabruno18@gmail.com>
2015-02-19 18:32:52 -0200

    M3Ex1

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -19,6 +19,14 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+        
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH, array($this, "onDispatch"), 100);
+    }
+    
+    public function onDispatch(MvcEvent $e)
+    {
+    	$viewModel = $e->getViewModel();
+    	$viewModel->setVariable("categories", "CATEGORY LIST");
     }
 
     public function getConfig()
